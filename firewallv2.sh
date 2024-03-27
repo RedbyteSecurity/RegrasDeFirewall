1f37f19ef0d7a6d094341e15e73caa9a25da40be
RedbyteSecurity <157855823+RedbyteSecurity@users.noreply.github.com>
2024-03-26 22:01:19 -0300

    Create firewallv2.sh

---
+++ firewallv2.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Rammon Pentest 
+# Limpa todas as regras existentes
+iptables -F
+iptables -X
+
+# Define a política padrão para DROP (descartar) para INPUT, FORWARD e OUTPUT
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT DROP
+
+# Permite conexões já estabelecidas e relacionadas
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# Bloqueia pings ICMP
+iptables -A INPUT -p icmp --icmp-type echo-request -j DROP
+
+# Bloqueia scanners de portas com pacotes TCP SYN
+iptables -A INPUT -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
+
+# Bloqueia varreduras de portas com pacotes TCP NULL, FIN e XMAS
+iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
+iptables -A INPUT -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
+
+# Bloqueia tentativas de identificação de versões de software pelo Nmap (-sV)
+iptables -A INPUT -m string --string "Nmap" --algo bm -j DROP
+
+# Permite tráfego de loopback (comunicação interna)
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+# Permite conexões SSH na porta 22
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+# Permite conexões HTTP na porta 80
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+
+# Exibe as regras configuradas
+iptables -L -v
+
+# Salva as regras para persistir após a reinicialização
+iptables-save > /etc/iptables/rules.v4
